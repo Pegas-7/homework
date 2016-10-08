2980718428ed1ac15b4619f88e251061cde3b3dd
Pegas7 <ifk88@mail.ru>
2016-10-08 20:10:45 +0300

    Added structure of homework on 07oct2016

---
+++ src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task2/ClassesAndObjects.java
@@ -0,0 +1,8 @@
+package com.yauhenikuntsevich.training.homework.hw07oct2016.task2;
+
+//Напишите приложение, позволяющее вести учет канцелярских товаров на рабочем месте сотрудника. 
+//Определите полную стоимость канцтоваров у определенного сотрудника.
+
+public class ClassesAndObjects {
+
+}
---
+++ src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task3/Inheritance.java
@@ -0,0 +1,7 @@
+package com.yauhenikuntsevich.training.homework.hw07oct2016.task3;
+
+//Разработайте иерархию канцелярских товаров. Создайте “набор новичка”, используя созданную иерархию.
+
+public class Inheritance {
+	
+}
