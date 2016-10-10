c2980d90dad4123d7ba486cab0a1f90a4f20abe8
Pegas7 <ifk88@mail.ru>
2016-10-10 19:06:35 +0300

    Set change on LinkedList, used method Collections.sort

--- src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task3and4/BegginerSet.java
+++ src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task3and4/BegginerSet.java
@@ -1,9 +1,6 @@
 package com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4;
 
-import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.Set;
-import java.util.TreeSet;
 
 import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.Stationery;
 import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.paperproduct.Notebook;
@@ -16,10 +13,10 @@ public class BegginerSet {
 
 	public BegginerSet() {
 		this.stationeries = new LinkedList<>();
-		stationeries.add(new Pen(7));
+		stationeries.add(new Pen(8));
 		stationeries.add(new Pencil(4));
-		stationeries.add(new PaperForPrint(15));
-		stationeries.add(new Notebook(11));
+		stationeries.add(new PaperForPrint(8));
+		stationeries.add(new Notebook(7));
 	}
 
 	public LinkedList<Stationery> getStationeries() {
--- src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task3and4/MainClass.java
+++ src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task3and4/MainClass.java
@@ -1,13 +1,11 @@
 package com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4;
 
+import java.util.Collections;
+
 import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.comporators.SortByItem;
 import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.comporators.SortByPrice;
 import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.comporators.SortByPriceAndItem;
 import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.Stationery;
-import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.paperproduct.Notebook;
-import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.paperproduct.PaperForPrint;
-import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.writingaccessories.Pen;
-import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.writingaccessories.Pencil;
 
 /**
  * Class specified for testing comparators
@@ -17,22 +15,25 @@ import com.yauhenikuntsevich.training.homework.hw07oct2016.task3and4.stationery.
  */
 public class MainClass {
 	public static void main(String[] args) {
-		BegginerSet begginerSet1 = new BegginerSet(new Pen(5), new Pencil(6), new PaperForPrint(15), new Notebook(15),
-				new SortByPrice());
+		BegginerSet begginerSet1 = new BegginerSet();
+		
+		Collections.sort(begginerSet1.getStationeries(), new SortByPrice());
 
 		for (Stationery stationery : begginerSet1.getStationeries()) {
 			System.out.println(stationery.getPrice() + " " + stationery.toString());
 		}
 
-		BegginerSet begginerSet2 = new BegginerSet(new Pen(5), new Pencil(3), new PaperForPrint(30), new Notebook(15),
-				new SortByItem());
+		BegginerSet begginerSet2 = new BegginerSet();
+		
+		Collections.sort(begginerSet2.getStationeries(), new SortByItem());
 
 		for (Stationery stationery : begginerSet2.getStationeries()) {
 			System.out.println(stationery.toString());
 		}
 
-		BegginerSet begginerSet3 = new BegginerSet(new Pen(5), new Pencil(5), new PaperForPrint(15), new Notebook(30),
-				new SortByPriceAndItem());
+		BegginerSet begginerSet3 = new BegginerSet();
+		
+		Collections.sort(begginerSet3.getStationeries(), new SortByPriceAndItem());
 
 		for (Stationery stationery : begginerSet3.getStationeries()) {
 			System.out.println(stationery.getPrice() + " " + stationery.toString());
