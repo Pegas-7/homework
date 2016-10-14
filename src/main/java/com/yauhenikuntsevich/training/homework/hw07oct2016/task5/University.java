a80cc40724e0d29e427a9f7a7b3e41975b789a8f
Pegas7 <ifk88@mail.ru>
2016-10-14 13:29:40 +0300

    no message

---
+++ src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task5/University.java
@@ -0,0 +1,7 @@
+package com.yauhenikuntsevich.training.homework.hw07oct2016.task5;
+
+public class University {
+	public static void main(String[] args) {
+		System.out.println("dfg");
+	}
+}
--- src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task7/AnnotationAnalyzer.java
+++ src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task7/AnnotationAnalyzer.java
@@ -1,7 +1,5 @@
 package com.yauhenikuntsevich.training.homework.hw07oct2016.task7;
 
-import java.lang.annotation.Annotation;
-
 public class AnnotationAnalyzer {
 	public void analyz(Class<?> clazz) throws Exception {
 		if (clazz.isAnnotationPresent(MyAnnotation.class)) {
