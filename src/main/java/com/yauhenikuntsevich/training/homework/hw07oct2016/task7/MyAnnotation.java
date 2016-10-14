4f493a49ac7f9c858ed2a4905ce5ccb01b1fac2f
Pegas7 <ifk88@mail.ru>
2016-10-14 10:28:56 +0300

    Correct annotation in task7

--- src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task7/MyAnnotation.java
+++ src/main/java/com/yauhenikuntsevich/training/homework/hw07oct2016/task7/MyAnnotation.java
@@ -1,5 +1,6 @@
 package com.yauhenikuntsevich.training.homework.hw07oct2016.task7;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +8,7 @@ import java.lang.annotation.Target;
 
 @Target(value = ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
+@Documented
 public @interface MyAnnotation {
 	String name1();
 }
